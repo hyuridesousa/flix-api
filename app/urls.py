816abf8e2cf826fda00d701e5211b046ddae316c
Yuri Paiva <hyuridesousa@gmail.com>
2024-11-26 11:06:33 -0300

    especificando as views

--- app/urls.py
+++ app/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_view
+from genres.views import genre_create_list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_view, name='genre-list'),
-   
+    path('genres/', genre_create_list_view, name='genre-create-list'),
+
 ]
